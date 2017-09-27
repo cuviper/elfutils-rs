0721f9a6d2506e1231c900dcebb1904b0b17c096
Josh Stone <jistone@redhat.com>
2017-09-27 10:13:07 -0700

    add gnu_debuglink

--- libdwelf/src/lib.rs
+++ libdwelf/src/lib.rs
@@ -1,9 +1,27 @@
 extern crate libdw_sys as ffi;
 extern crate libdw;
+extern crate libelf;
 
 use std::ffi::CStr;
 use std::ptr;
 use std::slice;
+
+#[inline]
+pub fn gnu_debuglink<'a>(elf: &libelf::Elf<'a>) -> libelf::Result<Option<(&'a CStr, u32)>> {
+    let mut crc = 0;
+    let namep = unsafe {
+        let elf = elf.as_ptr() as *mut _; // FIXME distinct bindgen Elf types
+        ffi::dwelf_elf_gnu_debuglink(elf, &mut crc)
+    };
+    if namep.is_null() {
+        match libelf::Error::check() {
+            Some(error) => Err(error),
+            None => Ok(None),
+        }
+    } else {
+        Ok(Some(unsafe { (CStr::from_ptr(namep), crc) }))
+    }
+}
 
 #[inline]
 pub fn gnu_debugaltlink<'a>(
