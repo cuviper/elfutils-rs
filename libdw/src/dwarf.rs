f876011d6b2e8f1601e0439bb9103633c34b5533
Josh Stone <jistone@redhat.com>
2017-09-26 21:50:05 -0700

    support gnu_debugaltlink

--- libdw/src/dwarf.rs
+++ libdw/src/dwarf.rs
@@ -1,7 +1,9 @@
 use ffi;
 use libelf;
 use std::ptr;
+use std::slice;
 
+use std::ffi::CStr;
 use std::marker::PhantomData;
 use std::os::unix::io::AsRawFd;
 
@@ -13,14 +15,16 @@ use super::Die;
 #[derive(Debug)]
 pub struct Dwarf<'a> {
     inner: *mut ffi::Dwarf,
+    owned: bool,
     phantom: PhantomData<&'a mut ffi::Dwarf>,
 }
 
 impl<'a> Dwarf<'a> {
     #[inline]
-    fn new(dwarf: *mut ffi::Dwarf) -> Dwarf<'a> {
+    fn new(dwarf: *mut ffi::Dwarf) -> Self {
         Dwarf {
             inner: dwarf,
+            owned: true,
             phantom: PhantomData,
         }
     }
@@ -33,10 +37,19 @@ impl<'a> Dwarf<'a> {
     }
 
     #[inline]
-    pub fn from_elf<'b>(elf: &'b libelf::Elf) -> Result<Dwarf<'b>> {
+    pub fn from_elf(elf: &'a libelf::Elf) -> Result<Self> {
         let elf = elf.as_ptr() as *mut _; // FIXME distinct bindgen Elf types
         let dwarf = ffi!(dwarf_begin_elf(elf, ffi::Dwarf_Cmd::DWARF_C_READ, ptr::null_mut()))?;
         Ok(Dwarf::new(dwarf))
+    }
+
+    #[inline]
+    pub unsafe fn from_raw(dwarf: *mut ffi::Dwarf) -> Self {
+        Dwarf {
+            inner: dwarf,
+            owned: false,
+            phantom: PhantomData,
+        }
     }
 
     #[inline]
@@ -71,6 +84,23 @@ impl<'a> Dwarf<'a> {
     }
 
     #[inline]
+    pub fn gnu_debugaltlink(&self) -> Result<Option<(&CStr, &[u8])>> {
+        let mut namep = ptr::null();
+        let mut build_idp = ptr::null();
+        let build_id_len = ffi!(
+            dwelf_dwarf_gnu_debugaltlink(self.as_ptr(), &mut namep, &mut build_idp)
+        )?;
+        if build_id_len > 0 {
+            Ok(Some(unsafe {
+                (CStr::from_ptr(namep),
+                 slice::from_raw_parts(build_idp as *const u8, build_id_len as usize))
+            }))
+        } else {
+            Ok(None)
+        }
+    }
+
+    #[inline]
     pub fn as_ptr(&self) -> *mut ffi::Dwarf {
         self.inner
     }
@@ -79,8 +109,10 @@ impl<'a> Dwarf<'a> {
 impl<'a> Drop for Dwarf<'a> {
     #[inline]
     fn drop(&mut self) {
-        unsafe {
-            ffi::dwarf_end(self.inner);
+        if self.owned {
+            unsafe {
+                ffi::dwarf_end(self.inner);
+            }
         }
     }
 }
