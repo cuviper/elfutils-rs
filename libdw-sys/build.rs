442139b45aa37c969aaed5c2ed29a6fee68c9896
Josh Stone <jistone@redhat.com>
2017-10-03 18:28:12 -0700

    add a pkg-config fallback

--- libdw-sys/build.rs
+++ libdw-sys/build.rs
@@ -1,5 +1,11 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::probe_library("libdw").unwrap();
+    assert!(pkg_config::target_supported());
+    if pkg_config::probe_library("libdw").is_err() {
+        // Guess!  This probably won't work in general, but it helps to have a
+        // shim for cases like docs.rs that don't really need to build.
+        println!("cargo:rustc-link-lib=dw");
+        println!("cargo:rustc-link-lib=elf");
+    }
 }
--- libelf-sys/build.rs
+++ libelf-sys/build.rs
@@ -1,5 +1,10 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::probe_library("libelf").unwrap();
+    assert!(pkg_config::target_supported());
+    if pkg_config::probe_library("libelf").is_err() {
+        // Guess!  This probably won't work in general, but it helps to have a
+        // shim for cases like docs.rs that don't really need to build.
+        println!("cargo:rustc-link-lib=elf");
+    }
 }
