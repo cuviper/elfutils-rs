92274d56667a91d088adacfa0f9ffcf5adbf5548
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-12-10 22:32:34 +0000

    Fix pkg_config::target_supported missing causing build error.
    On my machine (a raspberry pi 4 running Manjaro), I get an error saying that pkg_config doesn't contain target_supported.
    I'm not too familiar with rust or how to navigate it, but after a little digging it looks like it's inside the Config type.
    
    The docs for pkg_config (https://docs.rs/pkg-config/0.3.8/pkg_config/index.html) lead me to believe
    that it's fine to just new() up a Config and use that.
    
    Testing done:
    - cargo test
    - cargo test --release
    
    Output of uname -a:
    Linux pi 5.10.81-1-MANJARO-ARM-RPI #1 SMP PREEMPT Thu Nov 25 07:43:21 CST 2021 aarch64 GNU/Linux

--- libelf-sys/build.rs
+++ libelf-sys/build.rs
@@ -1,7 +1,7 @@
 extern crate pkg_config;
 
 fn main() {
-    assert!(pkg_config::target_supported());
+    assert!(pkg_config::Config::new().target_supported());
     if pkg_config::probe_library("libelf").is_err() {
         // Guess!  This probably won't work in general, but it helps to have a
         // shim for cases like docs.rs that don't really need to build.
