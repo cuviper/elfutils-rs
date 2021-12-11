38319c620985ba97fe3bee9eebd8ffc9555256e0
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-12-11 08:14:01 +0000

    Create a single Config for both the probe and target_supported calls.

--- libelf-sys/build.rs
+++ libelf-sys/build.rs
@@ -1,8 +1,9 @@
 extern crate pkg_config;
 
 fn main() {
-    assert!(pkg_config::Config::new().target_supported());
-    if pkg_config::probe_library("libelf").is_err() {
+    let config = pkg_config::Config::new();
+    assert!(config.target_supported());
+    if config.probe("libelf").is_err() {
         // Guess!  This probably won't work in general, but it helps to have a
         // shim for cases like docs.rs that don't really need to build.
         println!("cargo:rustc-link-lib=elf");
